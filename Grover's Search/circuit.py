17fa57af6bfc08da2a6420c91202ebda805e074a
TheDe-bugger <jaimildalwadi2003@gmail.com>
2021-03-31 11:16:21 -0400

    added Grover's search and organized

---
+++ Grover's Search/circuit.py
@@ -0,0 +1,27 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from numpy import pi
+
+qreg_q = QuantumRegister(2, 'q')
+creg_c = ClassicalRegister(2, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+
+circuit.h(qreg_q[0])
+circuit.h(qreg_q[1])
+circuit.z(qreg_q[0])
+circuit.z(qreg_q[1])
+circuit.p(pi/2, qreg_q[0])
+circuit.p(pi/2, qreg_q[1])
+circuit.barrier(qreg_q[1])
+circuit.barrier(qreg_q[0])
+circuit.h(qreg_q[1])
+circuit.h(qreg_q[0])
+circuit.z(qreg_q[1])
+circuit.z(qreg_q[0])
+circuit.p(pi/2, qreg_q[1])
+circuit.p(pi/2, qreg_q[0])
+circuit.h(qreg_q[1])
+circuit.h(qreg_q[0])
+circuit.barrier(qreg_q[1])
+circuit.barrier(qreg_q[0])
+circuit.measure(qreg_q[1], creg_c[1])
+circuit.measure(qreg_q[0], creg_c[0])
